582744ffe94315b79f8c629f4ca9fcbfaa2bc808
zmertens <zmertens@asu.edu>
2018-12-02 14:58:26 -0700

    Add future notes to android buidl script

--- examples/build_android_project.sh
+++ examples/build_android_project.sh
@@ -11,12 +11,33 @@
 # Download Android SDK (26+) and export path so we can use 'tools', 'platform-tools'
 # Download Android NDK (r15c+) and export path so we can use 'ndk-build'
 
-SDL2_0_9_Link='https://www.libsdl.org/release/SDL2-2.0.9.tar.gz'
+DIR_NAME="android_sdl2_example"
+SDL2_LATEST='https://www.libsdl.org/release/SDL2-2.0.9.tar.gz'
 
 # Example commands for setting up 'Dear ImGui' Android project
-wget $SDL2_0_9_Link && tar xzf SDL2-2.0.9.tar.gz -C SDL2
-cp -r SDL2/android-project/* ./imgui_example_android
-cd imgui_example_android/app/jni
-ln -s ../../../SDL2 ./SDL2
+wget -O "SDL2.tar.gz" $SDL2_LATEST && tar xzf SDL2.tar.gz
+mkdir $DIR_NAME
+cp -r ./SDL2/android-project/* ./$DIR_NAME
+cd $DIR_NAME/app/jni/src
+ln -s ../../../SDL2 ../SDL
+
+# Copy or Link Dear ImGui library into src directory
+
+# Copy necessary impl files that get imgui to work
+
+# Edit Application.mk so that c++_shared is being used, add exceptions if necessary
+
+# Edit the Android.mk files so that the CPP/ImGui files are being compiled
+
+# Edit the AndroidManifest app_name and activity, strings.xml, and create an extended SDLActivity
+# Edit the package names
+# Make sure this is on the Java side and not the JNI side
 
 # Need to link ImGui files
+
+
+# Build and Run commands
+./gradlew installDebug
+
+# adb commands:
+#  - adb devices, adb logcat
