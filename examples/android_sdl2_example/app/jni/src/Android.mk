73eec9a3761270298e5c7cfccc0ea034f91c9c3a
zmertens <zmertens@asu.edu>
2018-12-03 00:34:06 -0700

    Update Android example \ Remove impls, link using android makefile

--- examples/android_sdl2_example/app/jni/src/Android.mk
+++ examples/android_sdl2_example/app/jni/src/Android.mk
@@ -8,13 +8,16 @@ SDL_PATH := ../SDL
 
 IMGUI_PATH := ../../../../../
 
+IMGUI_IMPL_PATH := ../../../../
+
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
-	$(LOCAL_PATH)/$(IMGUI_PATH)
+	$(LOCAL_PATH)/$(IMGUI_PATH) \
+	$(LOCAL_PATH)/$(IMGUI_IMPL_PATH)
 
 # Add your application source files here...
 LOCAL_SRC_FILES := DearImGuiDemo.cpp \
-	imgui_impl_sdl.cpp \
-	imgui_impl_opengl3.cpp \
+	$(LOCAL_PATH)/$(IMGUI_IMPL_PATH)/imgui_impl_sdl.cpp \
+	$(LOCAL_PATH)/$(IMGUI_IMPL_PATH)/imgui_impl_opengl3.cpp \
 	$(LOCAL_PATH)/$(IMGUI_PATH)/imgui.cpp \
 	$(LOCAL_PATH)/$(IMGUI_PATH)/imgui_demo.cpp \
 	$(LOCAL_PATH)/$(IMGUI_PATH)/imgui_draw.cpp \
