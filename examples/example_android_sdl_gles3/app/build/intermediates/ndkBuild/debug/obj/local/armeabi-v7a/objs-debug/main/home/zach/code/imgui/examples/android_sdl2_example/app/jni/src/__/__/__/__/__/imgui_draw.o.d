5fd955f5d6c7058be8eb802dc0bbf8093148fed7
zmertens <zmertens@asu.edu>
2018-12-06 22:36:54 -0700

    Rename android example dir to follow other examples naming convention

---
+++ examples/example_android_sdl_gles3/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/main/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/__/__/__/__/__/imgui_draw.o.d
@@ -0,0 +1,41 @@
+/home/zach/code/imgui/examples/android_sdl2_example/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/main//home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/__/__/__/__/__//imgui_draw.o: \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../..//imgui_draw.cpp \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imgui.h \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imconfig.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/float.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/__config \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stddef.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/string.h \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imgui_internal.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/math.h \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imstb_textedit.h \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imstb_rectpack.h \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imstb_truetype.h
+
+/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imgui.h:
+
+/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imconfig.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/float.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/__config:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stddef.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imgui_internal.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/math.h:
+
+/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imstb_textedit.h:
+
+/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imstb_rectpack.h:
+
+/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imstb_truetype.h:
---
+++ examples/example_android_sdl_gles3/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/main/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/__/__/__/__/imgui_impl_opengl3.o.d
@@ -0,0 +1,26 @@
+/home/zach/code/imgui/examples/android_sdl2_example/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/main//home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/__/__/__/__//imgui_impl_opengl3.o: \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../..//imgui_impl_opengl3.cpp \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imgui.h \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imconfig.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/float.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/__config \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stddef.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/string.h \
+  /home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../imgui_impl_opengl3.h \
+  /home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stdio.h
+
+/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imgui.h:
+
+/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../../imconfig.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/float.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/__config:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stddef.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/home/zach/code/imgui/examples/android_sdl2_example/app/jni/src/../../../../imgui_impl_opengl3.h:
+
+/home/zach/Android/Ndk/sources/cxx-stl/llvm-libc++/include/stdio.h:
